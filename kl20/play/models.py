545e09c7a3ea0ce6a1c39ae80ed9d26c8ce2aa9e
Amit Sharma <amitksharma@gmail.com>
2025-09-26 11:53:35 +0000

    day 1: started models & views

--- kl20/play/models.py
+++ kl20/play/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Level(models.Model):
+    name = models.CharField(max_length=150)
+    question = models.CharField(max_length=200)
+    answer = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.name - self.question}"
+
+class Attempt(models.Model):
+    timestamp = models.DateTimeField(auto_now_add=True)
+    level = models.ForeignKey(Level, on_delete=models.CASCADE)
+    correct = models.BooleanField()
+    user = models.ForeignKey(User)
+
+    def __str__(self):
+        return f"{self.user} - {self.level}"
--- kl20/play/views.py
+++ kl20/play/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
+from .models import Level, Attempt
+# from .forms import CreateHabitForm, LogHabitForm
 
 # Create your views here.
+
+def main(request):
+    return HttpResponse('kl20')
+
+def Level(request, id):
+    thislevel = Level.objects.filter(id=id)
+    
