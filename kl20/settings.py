3078164043d9eb3b26b0cc0bca6e7c0812e9c195
Amit Sharma <amitksharma@gmail.com>
2026-02-17 06:45:49 +0000

    handling debug flag

--- kl20/settings.py
+++ kl20/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-import pathlib
+import pathlib, os
 from pathlib import Path
 
 settings_file_folder = pathlib.Path(__file__).parent.absolute()
@@ -25,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-w&$o0hn5)rt#t7(wkdvf&+()f0)c$k!wh3o@y7)o7=*1t5qhwa'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', 'True') == 'True'
+
 
 # MEDIA_ROOT = Path(__file__).resolve().parent / "media"
 MEDIA_ROOT = str(settings_file_folder / ".." / "media")
